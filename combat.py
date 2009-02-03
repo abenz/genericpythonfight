33047c305bab66d662c98c0901577549f27ae65a
Adam Benzan <clawtros@gmail.com>
2009-02-02 19:51:38 -0800

    make worky

--- combat.py
+++ combat.py
@@ -1,29 +1,25 @@
-from actions import *
+from action import *
 
 class Entity:
-    hp=1;max_hp=1
-    mp=0;max_mp=0
-    level=0
-    initiative=0
-    possible_actions=[]
-    location=(0,0,0)
+    def __init__(self, *args, **kwargs):
+        self.stats = {'hp'               : kwargs.get('hp',1),
+                      'max_hp'           : kwargs.get('max_hp',1),
+                      'mp'               : kwargs.get('mp',0),
+                      'max_mp'           : kwargs.get('max_mp',0),
+                      'level'            : kwargs.get('level',0),
+                      'initiative'       : kwargs.get('initiative',0),
+                      'possible_actions' : kwargs.get('possible_actions',[]),
+                      'location'         : kwargs.get('location',(0,0,0)),}
 
 class Combatant(Entity):
     def __init__(self,**kwargs):
-        self.possible_actions = [WaitAction(self)]
-        self.max_hp = kwargs.get('max_hp',1)
-        self.combatant_type = kwargs.get('combatant_type')
-        self.level = kwargs.get('level',1)
+        Entity.__init__(self,**kwargs)
     def act(self):
         pass
 
-class Soil(Entity):
-    pass
-
-class Tree(Entity):
-    pass
-
 class Monster(Combatant):
+    def __init__(self,**kwargs):
+        Entity.__init__(self,**kwargs)
     def act(self):
         action = self.possible_actions[randrange(0,len(possible_actions))]
         action.execute()
